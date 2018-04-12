a21c4df224cbb768a408cea8ba95717c8125312f
Robert Peters <423860+darpified@users.noreply.github.com>
2018-04-12 15:02:25 -0500

    Requires importing the catch operator
    Ch 11, listing 11-18
    Requires importing the catch operator or it throws an error.

--- Chapter 11/SportsStore/ClientApp/app/auth/authentication.service.ts
+++ Chapter 11/SportsStore/ClientApp/app/auth/authentication.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { Repository } from "../models/repository";
 import { Observable } from "rxjs/Observable";
 import { Router } from "@angular/router";
+import "rxjs/add/operator/catch";
 import "rxjs/add/observable/of";
 
 @Injectable()
